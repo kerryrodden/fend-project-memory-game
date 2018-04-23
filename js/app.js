8ed7f73bc5491374cc012344128c72e4d0cb2a70
Kerry Rodden <kerryrodden@users.noreply.github.com>
2018-04-23 15:08:20 -0700

    fix: Rubric says minimum score is 1 star

--- js/app.js
+++ js/app.js
@@ -107,8 +107,8 @@ function updateTurnDisplay() {
 };
 
 function updateScoreDisplay() {
-  // Maximum score of 3 stars is available if you take the smallest number of turns. After 32 turns, the score is 0.
-  score = Math.max(0, 3 - Math.floor(turnsTaken * 1.5 / cardCount));
+  // Maximum score of 3 stars is available if you take <16 turns. Then 2 stars for <32, 1 star for 32+
+  score = Math.max(1, 3 - Math.floor(turnsTaken / cardCount));
   console.log(turnsTaken, score);
   // Hide stars instead of removing them, so that the spacing remains consistent.
   document.querySelectorAll('.fa-star').forEach(function(element, index) {
@@ -132,15 +132,3 @@ function shuffle(array) {
 
   return array;
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */

898a3a85fc71967e697e4dea9c6621a83cff8774
Kerry Rodden <kerryrodden@users.noreply.github.com>
2018-04-24 13:45:58 -0700

    feat: Add modal dialog for winning state

--- js/app.js
+++ js/app.js
@@ -56,14 +56,14 @@ function initializeGame() {
   createNewGame(gameState);
   // Handle clicks and apply game logic (determine if open cards match)
   const deck = document.querySelector('.deck');
-  deck.addEventListener('click', function(event) {
+  deck.addEventListener('click', function (event) {
     if (event.target.classList.contains('card')) {
       handleCardClick(event.target, gameState);
     }
   });
   // Enable restart button
   const restart = document.querySelector('.restart');
-  restart.addEventListener('click', function() {
+  restart.addEventListener('click', function () {
     restartGame(gameState);
   });
 };
@@ -85,7 +85,6 @@ function handleCardClick(cardElement, gameState) {
       if (gameState.cardsInTurn[0] === gameState.cardsInTurn[1]) {
         showMatch();
         gameState.cardsCorrect += 2;
-        // TODO: check if cardsCorrect === cardCount
         gameState.cardsInTurn.length = 0;
       } else {
         setTimeout(closeOpenCards, 1000, gameState);
@@ -93,6 +92,7 @@ function handleCardClick(cardElement, gameState) {
       gameState.turnsTaken++;
       updateTurnDisplay(gameState.turnsTaken);
       updateScoreDisplay(gameState.turnsTaken);
+      checkForWinningState(gameState);
     }
   }
 };
@@ -129,9 +129,13 @@ function updateTurnDisplay(turnsTaken) {
   document.querySelector('.turns').textContent = turnsTaken + turnString;
 };
 
-function updateScoreDisplay(turnsTaken) {
+function calculateScore(turnsTaken) {
   // Maximum score of 3 stars is available if you take <16 turns. Then 2 stars for <32, 1 star for 32+
-  let score = Math.max(1, maxScore - Math.floor(turnsTaken / cardCount));
+  return Math.max(1, maxScore - Math.floor(turnsTaken / cardCount));
+}
+
+function updateScoreDisplay(turnsTaken) {
+  let score = calculateScore(turnsTaken);
   // Hide stars instead of removing them, so that the spacing remains consistent.
   document.querySelectorAll('.fa-star').forEach(function (element, index) {
     if (index + 1 > score) {
@@ -144,6 +148,21 @@ function updateTimerDisplay(startTime) {
   const elapsedSeconds = Math.round((Date.now() - startTime) / 1000);
   document.querySelector('.timer').textContent = elapsedSeconds + 's';
 };
+
+function checkForWinningState(gameState) {
+  if (gameState.cardsCorrect === cardCount) {
+    clearInterval(gameState.intervalId);
+    document.querySelector('.end-time').textContent = document.querySelector('.timer').textContent;
+    document.querySelector('.end-score').textContent = calculateScore(gameState.turnsTaken);
+    document.querySelector('.modal').classList.remove('hide');
+    document.querySelector('.overlay').classList.remove('hide');
+    document.querySelector('.play-again').addEventListener('click', function () {
+      document.querySelector('.modal').classList.add('hide');
+      document.querySelector('.overlay').classList.add('hide');
+      restartGame(gameState);
+    });
+  }
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
